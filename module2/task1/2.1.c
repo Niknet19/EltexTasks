985be561c5e1e291fc91fe1c82e73088b1a78651
Niknet19 <nikitos.feo12@gmail.com>
2024-10-22 17:03:56 +0700

    Начал задание 2.1

---
+++ module2/task1/2.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct Contact
+{
+    char *firstname;
+    char *surname;
+    char *workplace;
+    char *phone_number;
+    char *email;
+    char *socials_link;
+    char *messenger_page_link;
+} Contact;
+
+int main()
+{
+    printf("hello");
+    return 0;
+}
