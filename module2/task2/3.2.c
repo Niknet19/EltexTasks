7c36563cb536b4ae310a69c6a4ab4a2097ad56cf
Niknet19 <nikitos.feo12@gmail.com>
2024-11-02 20:41:21 +0700

    Улучшил консольный вывод.

--- module2/task2/3.2.c
+++ module2/task2/3.2.c
@@ -15,6 +15,7 @@
 #include <sys/types.h>
 #include <time.h>
 
+//Парсим строку в байтовое представление ip адресв
 u_int32_t parse_ip(const char* ip_as_str) {
     u_int32_t ip_as_uint = 0;
     long octets[4];
@@ -37,6 +38,7 @@ u_int32_t parse_ip(const char* ip_as_str) {
     return ip_as_uint;
 }
 
+//Проверка на принадлежность ip к подсети с маской mask
 int ip_check(u_int32_t network_ip, u_int32_t mask, u_int32_t ip_to_check) {
     u_int32_t subnetwork_ip = network_ip & mask;
     u_int32_t subnetwork_checked_ip = ip_to_check & mask;
@@ -52,14 +54,15 @@ int main(int argc, char* argv[]) {
     u_int32_t gateway_ip = parse_ip(argv[1]);
     u_int32_t subnet_musk_ip = parse_ip(argv[2]);
     int total_ips = atoi(argv[3]);
-    int subnet_ips = 0;
+    int ips_in_subnet = 0;
     srand((u_int32_t)time(NULL));
-    generate_random_ip();
     for (size_t i = 0; i < total_ips; i++) {
-        total_ips += ip_check(gateway_ip, subnet_musk_ip, generate_random_ip());
+        ips_in_subnet += ip_check(gateway_ip, subnet_musk_ip, generate_random_ip());
     }
-    printf("Принадлежат подсети: %d\n", subnet_ips);
+    printf("Принадлежат подсети: %d\n", ips_in_subnet);
+    printf("Другим сетям: %d\n", total_ips - ips_in_subnet);
     printf("Всего: %d\n", total_ips);
-    printf("Процент: %lf", (double)subnet_ips / (double)total_ips);
+
+    printf("Процент: %.3lf %%", ((double)ips_in_subnet / (double)total_ips) * 100.0);
     return 0;
 }
